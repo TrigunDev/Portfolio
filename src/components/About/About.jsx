168f2de770515861adcf612b6af66050ffbadedb
Trigun-123 <triguntamragouri.in>
2026-09-14 10:56:18 +0530

    Updated Resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
           </h3>
           {/* Resume Button */}
           {<a
-            href="https://drive.google.com/file/d/1a3PP3TxPGIeXN0nYHUGOjzjAjK8nEsnM/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Opfttqo9qQ3iafjBRvsj4ODey0fO8kSn/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
